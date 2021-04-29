18245fb2d8f41910716c67472d5b7ca3ac0de15e
Nathanael Ruf <nathanael97@protonmail.com>
2021-04-29 11:46:39 +0200

    Remove unused stuff. Add LICENSE.

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	// Disable searching for configuration files relative to the "filename" provided to Babel
-	babelrc: false,
-
-	presets: [
-		["@babel/preset-env", { modules: "commonjs", targets: { node: "current" } }],
-		"@babel/preset-typescript",
-	],
-};
